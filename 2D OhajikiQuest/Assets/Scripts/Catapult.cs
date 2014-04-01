8465b54f1b38da5b4a74100186b1d9e412bcee9e
Kentaro Ueda <uedakentarou0825@gmail.com>
2014-04-01 20:32:36 +0900

    カタパルト移動
    Catapult.cs
    制限付き移動

--- 2D OhajikiQuest/Assets/Scripts/Catapult.cs
+++ 2D OhajikiQuest/Assets/Scripts/Catapult.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class Catapult : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public float speed = 5.0f;
+    bool isFixed = false;
+
+	void Start () 
+    {
+        ChangeFixedCatapult();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        if (this.isFixed) // カタパルトが固定されているなら
+        {
+            MoveCatapult();
+        }
 	}
+
+    void MoveCatapult()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        if (Mathf.Sign(xAxis) < 0)
+        {
+            if (transform.position.x < -1.5f)
+            {
+                xAxis = 0;
+            }
+        }
+        else if (Mathf.Sign(xAxis) > 0)
+        {
+            if (transform.position.x > 1.5f)
+            {
+                xAxis = 0;
+            }
+        }
+        transform.Translate(Vector2.right * xAxis * this.speed * Time.deltaTime);
+    }
+
+    void ChangeFixedCatapult()
+    {
+        if (isFixed)
+        {
+            this.isFixed = false;
+        }
+        else
+        {
+            this.isFixed = true;
+        }
+    }
 }
