2167ceb5bf8818ef7eff2c1d9072a860bd34186f
Kentaro Ueda <uedakentarou0825@gmail.com>
2014-04-01 20:41:30 +0900

    Catapult.cs　Ball生成追加
    子オブジェクトとして生成

--- 2D OhajikiQuest/Assets/Scripts/Catapult.cs
+++ 2D OhajikiQuest/Assets/Scripts/Catapult.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Catapult : MonoBehaviour {
 
     public float speed = 5.0f;
+    public GameObject ball_P;
+    GameObject ball;
     bool isFixed = false;
 
 	void Start () 
     {
+        CreateBall();
         ChangeFixedCatapult();
 	}
 	
@@ -49,5 +52,12 @@ public class Catapult : MonoBehaviour {
         {
             this.isFixed = true;
         }
+    }
+
+    void CreateBall()
+    {
+        // 子としてBall生成
+        this.ball = Instantiate(this.ball_P, new Vector2(0, -2.5f), Quaternion.identity) as GameObject;
+        this.ball.transform.parent = gameObject.transform;
     }
 }
