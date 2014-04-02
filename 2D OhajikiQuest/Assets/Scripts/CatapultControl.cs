cb7db6b879af026eab06704d863629ca0a8f45c4
Kentaro Ueda <uedakentarou0825@gmail.com>
2014-04-02 21:54:35 +0900

    CatapultControl.cs作成
    Catapult.csとMouseOperation.csを1つにした。
    ターンフェイズ導入
    右クリックでFixedIconのテキストが変わる機能追加

---
+++ 2D OhajikiQuest/Assets/Scripts/CatapultControl.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatapultControl : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public GameObject ball_P;
+    GameObject ball;
+    GameObject phaseControl;
+    GameObject fixedIcon;
+    PhaseControl phaseComponent;
+    bool isFixedCatapult = false;
+    bool isMovingBall    = false;
+    bool isPulledBall    = false;
+
+    // フェイズ 0:待機 1:勇者 2:敵 
+    int phase = 0;
+    void Start()
+    {
+        this.phaseControl = GameObject.FindWithTag("PhaseControl");
+        this.fixedIcon    = GameObject.FindWithTag("FixedIcon");
+        CreateBall();
+    }
+
+    void Update()
+    {
+        GetPhase();
+        if (this.phase == 1) // 勇者フェイズ
+        {
+            if (!this.isMovingBall) // Ballが動いてないなら
+            {
+                if (!this.isPulledBall) // 引っ張り動作中でないなら
+                {
+                    if (Input.GetMouseButtonDown(1)) // 右クリックしたなら
+                    {
+                        Debug.Log("Mouse Right Button Down");
+                        ChangeFixedCatapult();
+                    }
+                }
+
+                if (this.isFixedCatapult) // Catapult固定中
+                {
+                    PullBall();
+                }
+                else
+                {
+                    MoveCatapult();
+                }
+            }
+        }
+    }
+
+    void GetPhase()
+    {
+        this.phaseComponent = this.phaseControl.GetComponent<PhaseControl>();
+        this.phase = this.phaseComponent.GetPhase();
+    }
+
+    void PullBall()
+    {
+        if (!this.isPulledBall) // 引っ張り動作中でないなら
+        {
+            if (Input.GetMouseButtonDown(0)) // 左クリック開始
+            {
+                Debug.Log("Mouse Left Button Down Start");
+                this.isPulledBall = true;
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0)) // 左クリック中
+            {
+                Debug.Log("Mouse Left Button Down");
+
+            }
+
+            if (Input.GetMouseButtonUp(0)) // 左クリック終了
+            {
+                Debug.Log("Mouse Left Button Up");
+                this.isPulledBall = false;
+                this.isMovingBall = true;
+                MoveBall();
+            }
+        }
+    }
+    void MoveCatapult()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        if (Mathf.Sign(xAxis) < 0)
+        {
+            if (transform.position.x < -1.5f)
+            {
+                xAxis = 0;
+            }
+        }
+        else if (Mathf.Sign(xAxis) > 0)
+        {
+            if (transform.position.x > 1.5f)
+            {
+                xAxis = 0;
+            }
+        }
+        transform.Translate(Vector2.right * xAxis * this.speed * Time.deltaTime);
+    }
+
+    void ChangeFixedCatapult()
+    {
+        if (this.isFixedCatapult)
+        {
+            this.isFixedCatapult = false;
+        }
+        else
+        {
+            this.isFixedCatapult = true;
+        }
+        this.fixedIcon.SendMessage("ChangeText");
+    }
+
+    void CreateBall()
+    {
+        // 子としてBall生成
+        this.ball = Instantiate(this.ball_P, new Vector2(0, -2.5f), Quaternion.identity) as GameObject;
+        this.ball.transform.parent = gameObject.transform;
+    }
+
+    void MoveBall()
+    {
+        Debug.Log("Move Ball Start");
+        // デバッグ用　ここから
+        this.isMovingBall = false;
+        ChangeFixedCatapult();
+        // デバッグ用　ここまで
+
+    }
+}
