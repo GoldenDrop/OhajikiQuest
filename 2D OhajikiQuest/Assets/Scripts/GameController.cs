bfe66841e4caa18e3bbb7f558bb1ef738dfb82f3
Kentaro Ueda <uedakentarou0825@gmail.com>
2014-04-11 21:44:37 +0900

    勝利条件　GameOver条件

---
+++ 2D OhajikiQuest/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    int enemyCount;
+
+
+    void GetEnemyCount(int count)
+    {
+        this.enemyCount = count;
+    }
+
+    void UpdateEnemyCount()
+    {
+        this.enemyCount--;
+        Debug.Log("<UpdateEnemyCount> Enemy Count : " + this.enemyCount);
+        if (this.enemyCount == 0)
+        {
+            Win();
+        }
+    }
+
+    void Win()
+    {
+        Debug.Log("YOU WIN");
+    }
+
+    void GameOver()
+    {
+        // シーン移動予定
+        Debug.Log("GameOver");
+    }
+}
