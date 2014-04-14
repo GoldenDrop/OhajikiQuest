422a5e19aaa36f041c69db8980bc3bec3271c68c
Kentaro Ueda <uedakentarou0825@gmail.com>
2014-04-14 21:35:42 +0900

    StageController作成
    Stageの生成、削除を行うオブジェクト

---
+++ 2D OhajikiQuest/Assets/Scripts/StageController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageController : MonoBehaviour {
+
+    int stageNumber = 1;
+    GameObject stage;
+
+	void Awake () {
+        CreateStage();
+	}
+
+    void CreateStage()
+    {
+        string path = "Stage/" + this.stageNumber;
+        Debug.Log("path : " + path);
+        GameObject stagePrefab = Resources.Load(path) as GameObject;
+        this.stage = Instantiate(stagePrefab, Vector2.zero, Quaternion.identity) as GameObject;
+
+    }
+
+    void NextStage()
+    {
+        this.stageNumber++;
+        if (this.stageNumber < 7)
+        {
+            Destroy(this.stage);
+            CreateStage();
+        }
+        else
+        {
+            Debug.Log("Game Clear");
+        }
+    }
+}
